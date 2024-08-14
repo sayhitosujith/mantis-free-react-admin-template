bdeb0b024416f40424402cc3adb477be327cec76
sujith.s <sujith.s@ringcentral.com>
2024-08-14 15:20:08 +0530

    changed texts

--- src/layout/Dashboard/Header/HeaderContent/Profile/index.jsx
+++ src/layout/Dashboard/Header/HeaderContent/Profile/index.jsx
@@ -91,7 +91,7 @@ export default function Profile() {
         <Stack direction="row" spacing={1.25} alignItems="center" sx={{ p: 0.5 }}>
           <Avatar alt="profile user" src={avatar1} size="sm" />
           <Typography variant="subtitle1" sx={{ textTransform: 'capitalize' }}>
-            John Doe
+            Sujith Reddy
           </Typography>
         </Stack>
       </ButtonBase>
@@ -124,9 +124,9 @@ export default function Profile() {
                         <Stack direction="row" spacing={1.25} alignItems="center">
                           <Avatar alt="profile user" src={avatar1} sx={{ width: 32, height: 32 }} />
                           <Stack>
-                            <Typography variant="h6">John Doe</Typography>
+                            <Typography variant="h6">Sujith Reddy</Typography>
                             <Typography variant="body2" color="text.secondary">
-                              UI/UX Designer
+                              SDET 2 at RingCentral
                             </Typography>
                           </Stack>
                         </Stack>
